2b8e61dbe43e61a94f4bb26dbc100a9215906c52
xavipapi <xaviparopi@gmail.com>
2018-04-10 15:11:03 +0200

    qwer

---
+++ holaMundo/holaMundo/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace holaMundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Click = new System.Windows.Forms.Button();
+            this.txt_Pass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Click
+            // 
+            this.btn_Click.Location = new System.Drawing.Point(102, 123);
+            this.btn_Click.Name = "btn_Click";
+            this.btn_Click.Size = new System.Drawing.Size(75, 23);
+            this.btn_Click.TabIndex = 0;
+            this.btn_Click.Text = "Click Aqui!";
+            this.btn_Click.UseVisualStyleBackColor = true;
+            this.btn_Click.Click += new System.EventHandler(this.btn_Click_Click);
+            // 
+            // txt_Pass
+            // 
+            this.txt_Pass.Location = new System.Drawing.Point(81, 77);
+            this.txt_Pass.Name = "txt_Pass";
+            this.txt_Pass.Size = new System.Drawing.Size(119, 20);
+            this.txt_Pass.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txt_Pass);
+            this.Controls.Add(this.btn_Click);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Click;
+        private System.Windows.Forms.TextBox txt_Pass;
+    }
+}
+
